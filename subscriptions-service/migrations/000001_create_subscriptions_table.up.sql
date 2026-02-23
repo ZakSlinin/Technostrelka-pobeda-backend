2b5feda62ae703221b2e1bfad5cd16906177c70a
Zak Slinin <156526564+ZakSlinin@users.noreply.github.com>
2026-02-23 15:53:15 +0300

    feat: migrations for subscriptions

---
+++ subscriptions-service/migrations/000001_create_subscriptions_table.up.sql
@@ -0,0 +1,25 @@
+CREATE TYPE subscription_status AS ENUM ('paid', 'unpaid');
+
+CREATE TABLE subscriptions (
+   subscription_id UUID NOT NULL,
+   user_id UUID NOT NULL,
+
+   name TEXT NOT NULL,
+   cost NUMERIC(10,2) NOT NULL CHECK (cost >= 0),
+
+   next_billing TIMESTAMP NOT NULL,
+
+   status subscription_status NOT NULL DEFAULT 'unpaid',
+
+   subscription_avatar_url TEXT,
+   category TEXT,
+   url_service TEXT,
+
+   FOREIGN KEY (user_id)
+       REFERENCES users(id)
+       ON DELETE CASCADE
+);
+
+CREATE INDEX idx_subscriptions_user_id ON subscriptions(user_id);
+CREATE INDEX idx_subscriptions_status ON subscriptions(status);
+CREATE INDEX idx_subscriptions_next_billing ON subscriptions(next_billing);
