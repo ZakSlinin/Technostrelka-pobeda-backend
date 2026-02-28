e0c0bef3a01dc792624422741be27f76cfb5879c
Zak Slinin <156526564+ZakSlinin@users.noreply.github.com>
2026-02-28 17:42:22 +0300

    feat: CreateSubscriptionRequest model

--- subscriptions-service/model/subscription_model.go
+++ subscriptions-service/model/subscription_model.go
@@ -24,7 +24,21 @@ type UpdateSubscriptionRequest struct {
 	NextBilling        *time.Time `json:"next_billing"`
 	SubscriptionAvatar *string    `json:"subscription_avatar"`
 	Category           *string    `json:"category"`
-	URLService         *string    `json:"url_service"`
+	UrlService         *string    `json:"url_service"`
 	UseInThisMonth     *bool      `json:"use_in_this_month"`
 	CancellationLink   *string    `json:"cancellation_link"`
+}
+
+type CreateSubscriptionRequest struct {
+	SubscriptionID         string    `gorm:"column:subscription_id" json:"subscription_id"`
+	UserID                 string    `gorm:"column:user_id" json:"user_id"`
+	Name                   string    `gorm:"column:name" json:"name"`
+	Cost                   float64   `gorm:"column:cost" json:"cost"`
+	NextBilling            time.Time `gorm:"column:next_billing" json:"next_billing"`
+	Status                 bool      `gorm:"column:status" json:"status"`
+	SubscriptionsAvatarUrl *string   `gorm:"column:subscription_avatar_url" json:"subscription_avatar_url"`
+	Category               string    `gorm:"column:category" json:"category"`
+	UrlService             string    `gorm:"column:url_service" json:"url_service"`
+	UseInThisMonth         bool      `gorm:"column:use_in_this_month" json:"use_in_this_month"`
+	CancellationLink       string    `gorm:"column:cancellation_link" json:"cancellation_link"`
 }
