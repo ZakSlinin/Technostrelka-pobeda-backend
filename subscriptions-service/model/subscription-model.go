4be0283615d5aef25e9dcfc62cd7db504c29f9a7
Zak Slinin <156526564+ZakSlinin@users.noreply.github.com>
2026-03-03 20:41:15 +0300

    update: subscription model - add form tag

--- subscriptions-service/model/subscription-model.go
+++ subscriptions-service/model/subscription-model.go
@@ -20,24 +20,23 @@ type Subscriptions struct {
 }
 
 type UpdateSubscriptionRequest struct {
-	Name               *string    `json:"name"`
-	Cost               *float64   `json:"cost"`
-	NextBilling        *time.Time `json:"next_billing"`
-	SubscriptionAvatar *string    `json:"subscription_avatar"`
-	Category           *string    `json:"category"`
-	UrlService         *string    `json:"url_service"`
-	UseInThisMonth     *bool      `json:"use_in_this_month"`
-	CancellationLink   *string    `json:"cancellation_link"`
+	Name             *string      `form:"name" json:"name"`
+	Cost             *float64     `form:"cost" json:"cost"`
+	NextBilling      *ISO8601Time `form:"next_billing" json:"next_billing"`
+	Category         *string      `form:"category" json:"category"`
+	UrlService       *string      `form:"url_service" json:"url_service"`
+	UseInThisMonth   *bool        `form:"use_in_this_month" json:"use_in_this_month"`
+	CancellationLink *string      `form:"cancellation_link" json:"cancellation_link"`
+	Status           *bool        `form:"status" json:"status"`
 }
 
 type CreateSubscriptionRequest struct {
-	Name                   string      `gorm:"column:name" json:"name"`
-	Cost                   float64     `gorm:"column:cost" json:"cost"`
-	NextBilling            ISO8601Time `gorm:"column:next_billing" json:"next_billing"`
-	Status                 bool        `gorm:"column:status" json:"status"`
-	SubscriptionsAvatarUrl string      `gorm:"column:subscription_avatar_url" json:"subscription_avatar_url"`
-	Category               string      `gorm:"column:category" json:"category"`
-	UrlService             string      `gorm:"column:url_service" json:"url_service"`
-	UseInThisMonth         bool        `gorm:"column:use_in_this_month" json:"use_in_this_month"`
-	CancellationLink       string      `gorm:"column:cancellation_link" json:"cancellation_link"`
+	Name             string      `form:"name" json:"name"`
+	Cost             float64     `form:"cost" json:"cost"`
+	NextBilling      ISO8601Time `form:"next_billing" json:"next_billing"`
+	Status           bool        `form:"status" json:"status"`
+	Category         string      `form:"category" json:"category"`
+	UrlService       string      `form:"url_service" json:"url_service"`
+	UseInThisMonth   bool        `form:"use_in_this_month" json:"use_in_this_month"`
+	CancellationLink string      `form:"cancellation_link" json:"cancellation_link"`
 }
