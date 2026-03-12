d2e6bbbede386eaf77570d889cbfc0bedfe158b3
gshorti <ilazajkin52969@gmail.com>
2026-03-12 20:00:59 +0300

    add user_is_connected_email field to user

--- user-service/backend/user/models.py
+++ user-service/backend/user/models.py
@@ -9,6 +9,7 @@ class User(AbstractUser):
 	fullname = models.CharField(max_length=100, blank=False, null=False)
 	avatar = models.FileField(upload_to="avatars", blank=False, null=False)
 	notifications = models.BooleanField(default=True)
+	is_connected_email =models.BooleanField(default=False)
 	is_active = True
 
 	def __str__(self):
